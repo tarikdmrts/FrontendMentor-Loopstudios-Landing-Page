62a57c314dc46e8d622261b6f199955bb898d03b
Tarık Demirtaş <tarikdemirtas@Tark-MacBook-Air.local>
2025-09-15 14:52:58 +0300

    Desktop design

--- src/components/Creations/Creations.jsx
+++ src/components/Creations/Creations.jsx
@@ -1,11 +1,21 @@
-import deepEarth from "../../images/mobile/image-deep-earth.jpg";
-import nightArcade from "../../images/mobile/image-night-arcade.jpg";
-import soccerTeam from "../../images/mobile/image-soccer-team.jpg";
-import theGrid from "../../images/mobile/image-grid.jpg";
-import fromUpAbove from "../../images/mobile/image-from-above.jpg";
-import pocketBorealis from "../../images/mobile/image-pocket-borealis.jpg";
-import theCuriosity from "../../images/mobile/image-curiosity.jpg";
-import makeItFisheye from "../../images/mobile/image-fisheye.jpg";
+import deepEarthMobile from "../../images/mobile/image-deep-earth.jpg";
+import nightArcadeMobile from "../../images/mobile/image-night-arcade.jpg";
+import soccerTeamMobile from "../../images/mobile/image-soccer-team.jpg";
+import theGridMobile from "../../images/mobile/image-grid.jpg";
+import fromUpAboveMobile from "../../images/mobile/image-from-above.jpg";
+import pocketBorealisMobile from "../../images/mobile/image-pocket-borealis.jpg";
+import theCuriosityMobile from "../../images/mobile/image-curiosity.jpg";
+import makeItFisheyeMobile from "../../images/mobile/image-fisheye.jpg";
+
+import deepEarthDesktop from "../../images/desktop/image-deep-earth.jpg";
+import nightArcadeDesktop from "../../images/desktop/image-night-arcade.jpg";
+import soccerTeamDesktop from "../../images/desktop/image-soccer-team.jpg";
+import theGridDesktop from "../../images/desktop/image-grid.jpg";
+import fromUpAboveDesktop from "../../images/desktop/image-from-above.jpg";
+import pocketBorealisDesktop from "../../images/desktop/image-pocket-borealis.jpg";
+import theCuriosityDesktop from "../../images/desktop/image-curiosity.jpg";
+import makeItFisheyeDesktop from "../../images/desktop/image-fisheye.jpg";
+
 import "./creations.scss";
 
 export default function Creations() {
@@ -16,40 +26,107 @@ export default function Creations() {
       </div>
       <div className="creations__list">
         <div className="creations__item">
-          <img src={deepEarth} className="creations__item__image"></img>
+          <picture>
+            <source media="(min-width: 1024px)" srcSet={deepEarthDesktop} />
+            <img
+              src={deepEarthMobile}
+              className="creations__item__image"
+              alt="Deep Earth"
+            />
+          </picture>
           <span className="creations__item__header">Deep Earth</span>
         </div>
+
         <div className="creations__item">
-          <img src={nightArcade} className="creations__item__image"></img>
+          <picture>
+            <source media="(min-width: 1024px)" srcSet={nightArcadeDesktop} />
+            <img
+              src={nightArcadeMobile}
+              className="creations__item__image"
+              alt="Night Arcade"
+            />
+          </picture>
           <span className="creations__item__header">Night Arcade</span>
         </div>
+
         <div className="creations__item">
-          <img src={soccerTeam} className="creations__item__image"></img>
+          <picture>
+            <source media="(min-width: 1024px)" srcSet={soccerTeamDesktop} />
+            <img
+              src={soccerTeamMobile}
+              className="creations__item__image"
+              alt="Soccer Team VR"
+            />
+          </picture>
           <span className="creations__item__header">Soccer Team VR</span>
         </div>
+
         <div className="creations__item">
-          <img src={theGrid} className="creations__item__image"></img>
-          <span className="creations__item__header">The</span>
-          <span className="creations__item__header grid">Grid</span>
+          <picture>
+            <source media="(min-width: 1024px)" srcSet={theGridDesktop} />
+            <img
+              src={theGridMobile}
+              className="creations__item__image"
+              alt="The Grid"
+            />
+          </picture>
+          <span className="creations__item__header grid">The Grid</span>
         </div>
+
         <div className="creations__item">
-          <img src={fromUpAbove} className="creations__item__image"></img>
+          <picture>
+            <source media="(min-width: 1024px)" srcSet={fromUpAboveDesktop} />
+            <img
+              src={fromUpAboveMobile}
+              className="creations__item__image"
+              alt="From Up Above VR"
+            />
+          </picture>
           <span className="creations__item__header">From Up Above VR</span>
         </div>
+
         <div className="creations__item">
-          <img src={pocketBorealis} className="creations__item__image"></img>
+          <picture>
+            <source
+              media="(min-width: 1024px)"
+              srcSet={pocketBorealisDesktop}
+            />
+            <img
+              src={pocketBorealisMobile}
+              className="creations__item__image"
+              alt="Pocket Borealis"
+            />
+          </picture>
           <span className="creations__item__header">Pocket Borealis</span>
         </div>
+
         <div className="creations__item">
-          <img src={theCuriosity} className="creations__item__image"></img>
+          <picture>
+            <source media="(min-width: 1024px)" srcSet={theCuriosityDesktop} />
+            <img
+              src={theCuriosityMobile}
+              className="creations__item__image"
+              alt="The Curiosity"
+            />
+          </picture>
           <span className="creations__item__header">The Curiosity</span>
         </div>
+
         <div className="creations__item">
-          <img src={makeItFisheye} className="creations__item__image"></img>
+          <picture>
+            <source media="(min-width: 1024px)" srcSet={makeItFisheyeDesktop} />
+            <img
+              src={makeItFisheyeMobile}
+              className="creations__item__image"
+              alt="Make It Fisheye"
+            />
+          </picture>
           <span className="creations__item__header">Make It Fisheye</span>
         </div>
       </div>
-      <button className="creations__button">See All</button>
+      <div className="creations__button-container">
+        <button className="creations__button">See All</button>
+      </div>
     </section>
   );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,28 +1,52 @@
-import './footer.scss';
-import loopStudiosLogo from '../../images/logo.svg';
-import facebookIcon from '../../images/icon-facebook.svg';
-import twitterIcon from '../../images/icon-twitter.svg';
-import pinterestIcon from '../../images/icon-pinterest.svg';
-import instagramIcon from '../../images/icon-instagram.svg';
+import "./footer.scss";
+import loopStudiosLogo from "../../images/logo.svg";
+import facebookIcon from "../../images/icon-facebook.svg";
+import twitterIcon from "../../images/icon-twitter.svg";
+import pinterestIcon from "../../images/icon-pinterest.svg";
+import instagramIcon from "../../images/icon-instagram.svg";
 
-export default function Footer(){
-    return (
-        <footer className="footer">
-            <img src={loopStudiosLogo} alt="Loopstudios Logo" className="footer__logo" />
-            <ul className="footer__list">
-                <li className="footer__item">About</li>
-                <li className="footer__item">Careers</li>
-                <li className="footer__item">Events</li>
-                <li className="footer__item">Products</li>
-                <li className="footer__item">Support</li>
-            </ul>
-            <div className="footer__socials">
-                <img src={facebookIcon} alt="Facebook Icon" className="footer__social-icon" />
-                <img src={twitterIcon} alt="Twitter Icon" className="footer__social-icon" />
-                <img src={pinterestIcon} alt="Pinterest Icon" className="footer__social-icon" />
-                <img src={instagramIcon} alt="Instagram Icon" className="footer__social-icon" />
-            </div>
-            <p className="footer__text">© 2025 Loopstudios. All rights reserved.</p>
-        </footer>
-    );
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__divider">
+        <img
+          src={loopStudiosLogo}
+          alt="Loopstudios Logo"
+          className="footer__logo"
+        />
+        <ul className="footer__list">
+          <li className="footer__item">About</li>
+          <li className="footer__item">Careers</li>
+          <li className="footer__item">Events</li>
+          <li className="footer__item">Products</li>
+          <li className="footer__item">Support</li>
+        </ul>
+      </div>
+      <div className="footer__bottom">
+        <div className="footer__socials">
+          <img
+            src={facebookIcon}
+            alt="Facebook Icon"
+            className="footer__social-icon"
+          />
+          <img
+            src={twitterIcon}
+            alt="Twitter Icon"
+            className="footer__social-icon"
+          />
+          <img
+            src={pinterestIcon}
+            alt="Pinterest Icon"
+            className="footer__social-icon"
+          />
+          <img
+            src={instagramIcon}
+            alt="Instagram Icon"
+            className="footer__social-icon"
+          />
+        </div>
+        <p className="footer__text">© 2025 Loopstudios. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 }
